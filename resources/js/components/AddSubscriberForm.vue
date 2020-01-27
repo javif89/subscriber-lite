7bc39362fe63d63c9064eb3e9a7c4013fbd2f0b1
javif89 <javier0eduardo@hotmail.com>
2020-01-26 23:30:52 -0500

    Add dns checking to email

--- resources/js/components/AddSubscriberForm.vue
+++ resources/js/components/AddSubscriberForm.vue
@@ -49,8 +49,8 @@ export default {
   data() {
     return {
       subscriber: {
-        name: "John",
-        email: "john@john.com",
+        name: "",
+        email: "",
         state: "active"
       },
       loading: false
